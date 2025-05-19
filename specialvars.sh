afc734939843f27ca45f37293b1792fbb4d9cbd8
mallu-harshavardhan07 <malluharshavardhanreddy07@gmail.com>
2025-05-19 19:59:33 +0530

    sepcial vars

--- specialvars.sh
+++ specialvars.sh
@@ -1,4 +1,10 @@
 
 #!/bin/bash
 
-echo "helloworld"
+echo "prints all the variables passes to the script : $@"
+echo "prints number of varibales : $# "
+echo "prints the user using script :$USER"
+echo "print the home directioru of user :$HOME"
+echo "pid of the current script : $$"
+echo "current running directory : $PWD "
+echo "Script name : $0 "
