73be91123503605f7b0587a12fc9d364c3048486
mallu-harshavardhan07 <malluharshavardhanreddy07@gmail.com>
2025-06-08 13:03:07 +0530

    message

---
+++ 03-variables.sh
@@ -0,0 +1,10 @@
+
+#!/bin/bash
+PERSON1=MOM
+PERSON2=HARSHA
+
+
+echo "$PERSON1 : come to temple ."
+echo "$PERSON2: already went right again why "
+echo "$PERSON1: shutup and come"
+echo "$PERSON2: alright "
