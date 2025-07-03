98ece5284305d3d40944e404e0c08cbdec677387
mallu-harshavardhan07 <malluharshavardhanreddy07@gmail.com>
2025-07-03 07:41:35 +0530

    changing

--- 10-install_command.sh
+++ 10-install_command.sh
@@ -7,3 +7,10 @@ then
 else
     echo "running with root access "
 fi
+dnf install mysql -y
+if [ $? -eq 0 ]
+then
+    echo " installing my sql is success "
+else 
+    echo " mysql is NOT INSTALLED . try again "
+fi
