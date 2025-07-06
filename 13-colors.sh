efee8b363b075a7e8fd291112370769124d95e0e
mallu-harshavardhan07 <malluharshavardhanreddy07@gmail.com>
2025-07-06 16:09:28 +0530

    message

---
+++ 13-colors.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+red='\e[31m'
+green='\e[32m'
+yellow='\e[33m'
+reset='\e[0m'
+
+echo -e "$red this is red text $reset"
+echo -e "$green this is green text $reset "
+echo -e "$yellow this is yellow text $reset "
