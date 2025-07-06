951cd9e20aeca8424112835e57e776624330fdb1
mallu-harshavardhan07 <malluharshavardhanreddy07@gmail.com>
2025-07-06 16:15:50 +0530

    message

--- 14-colors2.sh
+++ 14-colors2.sh
@@ -8,17 +8,17 @@ reset='\e[0m'
 userid=$(id -u)
 if [ $userid -eq 0 ]
 then
-    echo " $green you are running with root access $reset "
+    echo " ${green} you are running with root access ${reset} "
 else
-    echo " $red eror :$reset you are not running with root access "
+    echo " ${red} eror :${reset} you are not running with root access "
     exit 1
 fi
 validate(){
     if [ $1 -eq 0 ]
     then
-        echo " $green installing $2 is success $reset "
+        echo " ${green} installing $2 is success ${reset} "
     else
-        echo " $red error: $reset occured while installing $2"
+        echo " ${red} error: ${reset} occured while installing $2"
         exit 1
     fi
 } 
@@ -26,19 +26,19 @@ validate(){
 dnf list installed nginx 
 if [ $? -ne 0 ]
 then
-    echo " $green you are installing nginx  $reset "
+    echo " ${green} you are installing nginx  ${reset} "
     dnf install nginx  -y 
     validate $? "nginx"
 else 
-     echo "$yellow nginx  is already installed $reset "
+     echo "${yellow} nginx  is already installed ${reset} "
 fi
 
 dnf list installed mysql
 if [ $? -ne 0 ]
 then
-    echo " $green you are installing mysql  $reset "
+    echo " ${green} you are installing mysql  ${reset} "
     dnf install mysql  -y
     validate $? "mysql"
 else 
-     echo "$yellow mysql  is aready installed $reset"
+     echo "${yellow} mysql  is aready installed ${reset}"
 fi
